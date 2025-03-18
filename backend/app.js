049420c9b3e18572aac8f9d11f0095c99b1b2b33
Lucivando Sousa <lucivandosousa@gmail.com>
2025-03-18 12:46:16 -0300

    feat: create setup project

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Musicalizar está on!',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
