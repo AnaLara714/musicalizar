fa0351d0229db4360a8f9477ba35e369445d036f
Lucivando Sousa <lucivandosousa@gmail.com>
2025-03-20 17:05:43 -0300

    fix: remove login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,6 @@ function App() {
     <div className="flex flex-col h-screen ">
       <Header />
       <SectionAboutUs />
-     /* <div className="flex flex-1 justify-center items-center bg-sky-400" >
-        <Login />
-      </div>*/
       <Footer />
     </div>
   )
